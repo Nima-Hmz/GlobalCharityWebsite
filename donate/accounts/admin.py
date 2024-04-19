d9c1d057166ee3fa6f3fec2ff7a2175bd48901bb
Nima-Hmz <hmzmain11@gmail.com>
2024-04-19 11:36:38 -0700

    user model added

--- donate/accounts/admin.py
+++ donate/accounts/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import *
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import *
+from django.contrib.auth.models import Group
 
-# Register your models here.
+
+
+@admin.register(OtpCodeModel)
+class OtpCodeAdmin(admin.ModelAdmin):
+    list_display = ['phone_number' , 'otp' , 'created']
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreateinfoForm
+
+    list_display = ['phone_number' , 'display_name', 'total_donate' , 'full_name' , 'is_admin']
+    list_filter = ['is_admin']
+    search_fields = ['phone_number' , 'full_name']
+
+    fieldsets = (
+            (None , {'fields' : ('phone_number' , 'display_name', 'total_donate' , 'full_name' , 'password')}),
+            ('Permissions' , {'fields' : ('is_active' , 'is_admin', 'last_login')}),
+    )
+
+    add_fieldsets = (
+        (None , {'fields':('phone_number' , 'display_name', 'total_donate' , 'full_name' , 'password1' , 'password2')}),
+    )
+
+
+    ordering = ['full_name']
+    filter_horizontal = ()
+
+admin.site.unregister(Group)
+admin.site.register(User , UserAdmin)
--- donate/accounts/models.py
+++ donate/accounts/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    phone_number = models.CharField(max_length=11 , unique=True)
+    full_name = models.CharField(max_length=124)
+    display_name = models.BooleanField(default=True)
+    total_donate = models.PositiveBigIntegerField(default=0)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['full_name', 'display_name']
+
+    def __str__(self) :
+        return self.phone_number
+    
+    def has_perm(self , perm , obj=None):
+        return True
+    
+    def has_module_perms(self , app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin
+        
+
+class OtpCodeModel(models.Model):
+    phone_number = models.CharField(max_length=11)
+    otp          = models.PositiveSmallIntegerField()
+    created      = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f'{self.phone_number} - {self.otp} - {self.created}'
