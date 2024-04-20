2b856589a428b12c88f08500504b78d56b7133c3
Nima-Hmz <hmzmain11@gmail.com>
2024-04-20 13:12:16 -0700

    register logic and templates are added

--- donate/accounts/urls.py
+++ donate/accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("user_login/" , UserLoginView.as_view() , name='user_login'),
     path("user_logout/", UserLogoutView.as_view(), name='user_logout'),
     path("user_register/", UserRegisterView.as_view(), name='user_register'),
+    path("user_register_vertify/", UserRegisterVertifyView.as_view(), name='register_vertify'),
 ]
--- donate/accounts/views.py
+++ donate/accounts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from utils import send_otp_code
+import random
+from .models import User, OtpCodeModel
 from django.contrib import messages
+from django.utils import timezone
+import datetime as my_datetime
 from django.views import View
 
 # User Login view.
@@ -43,5 +48,96 @@ class UserLogoutView(View):
 
 
 class UserRegisterView(View):
-    pass 
+    def get(self , request):
+        return render(request, 'accounts/register.html')
+    
+    def post(self, request):
+        username_filed = request.POST.get('full_name')
+        phone_number = request.POST.get('phone_number')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('conpass')
+        display_name = request.POST.get('display_name')
+
+        if password == confirm_password:
+            if User.objects.filter(phone_number=phone_number).exists():
+                messages.error(request, "شماره موبایل تکراری است", 'danger')
+                return redirect("account:user_register")
+            else:
+                if len(phone_number) > 11 or len(phone_number) < 10:
+                    messages.error(request, "شماره موبایل باید 11 رقم باشد", 'danger')
+                    return redirect("account:user_register")
+                else:
+                    if OtpCodeModel.objects.filter(phone_number=phone_number).exists():
+                        messages.error(request, "دوباره تلاش کنید", 'danger')
+                        OtpCodeModel.objects.filter(phone_number=phone_number).delete()
+                        return redirect("account:user_register")
+                    else:
+                        reandom_code = random.randint(1000, 9999)
+                        print(f"-------\n{reandom_code}\n---------")
+                        send_otp_code(phone_number=phone_number, code=reandom_code)
+                        OtpCodeModel.objects.create(phone_number= phone_number, otp=reandom_code)
+                        request.session["user_registration_info"] = {
+
+                            'phone_number':phone_number,
+                            'display_name':display_name,
+                            'username':username_filed,
+                            'password':password,
+
+                        }
+                        messages.success(request, "کد را برای شما از طریق پیامک ارسال کردیم", 'success')
+                        return redirect("account:register_vertify")
+                    
+
+class UserRegisterVertifyView(View):
+
+    def get(self, request):
+        try:
+            x = request.session['user_registration_info'] 
+            return render(request, "accounts/register_vertify.html")
+
+        except:
+            messages.error(request, "خطا دوباره تلاش کنید", 'danger')
+            return redirect("home:index")
+        
+    def post(self, request):
+        try:
+            user_session = request.session['user_registration_info']
+            code_instance = OtpCodeModel.objects.get(phone_number=user_session['phone_number'])
+            user_input = int(request.POST.get("vertifycode"))
+        except:
+            messages.error(request, "خطا دوباره تلاش کنید", 'danger')
+            return redirect("home:index")
+
+
+        if user_input == code_instance.otp:
+            temp = code_instance.created
+            temp += my_datetime.timedelta(seconds=120)
+            now = timezone.now()
+            if now < temp:
+
+                # user = User.objects.create_user(username=user_session['username'], email=user_session['email'], password=user_session['password'])
+                # user.save()
+                # data = Customer(user=user, phone_number=user_session["phone_number"])
+                # data.save()
+                display_name1 = not bool(user_session['display_name'])
+
+                user = User.objects.create_user(phone_number=user_session['phone_number'], full_name=user_session['username'], display_name=display_name1, password=user_session['password'])
+                user.save()
+                code_instance.delete()
+                messages.success(request, "حساب کاربری شما ایجاد شد", 'success')
+
+                #  login after register 
+                our_user = authenticate(username=user_session['phone_number'], password=user_session['password'])
+                if our_user is not None:
+                    login(request, user)
+                    return redirect("home:index")
+              
+            else:
+                code_instance.delete()
+                messages.error(request, "زمان شما به اتمام رسید", 'danger')
+                return redirect('account:user_register')
+
+        else:
+            messages.error(request, "کد وارد شده اشتباه است", 'danger')
+            return redirect('account:register_vertify')
 
