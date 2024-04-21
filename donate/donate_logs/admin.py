e862ce0641985976102bbb361d531b98e1898680
Nima-Hmz <hmzmain11@gmail.com>
2024-04-21 11:48:04 -0700

    donate log model and admin added

--- donate/donate_logs/admin.py
+++ donate/donate_logs/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import DonateLog
 
 # Register your models here.
+
+
+
+@admin.register(DonateLog)
+class DonateLogAdmin(admin.ModelAdmin):
+    list_display = ['user', 'amount' , 'date']
+    search_fields = ['amount']
--- donate/donate_logs/models.py
+++ donate/donate_logs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+
+
+class DonateLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="donator")
+    amount = models.PositiveBigIntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.amount}'
+    
+
+    class Meta:
+        ordering = ('-date',)
